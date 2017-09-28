d64c566b38eb0e7dee0553a8ee1efd320b88b9eb
Imac-marc <marc.arnoult@hotmail.fr>
2017-09-28 19:12:35 +0200

    who is near finish

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -13,6 +13,19 @@ class HomeController extends Controller
      */
     public function indexAction(Request $request)
     {
-        return $this->render('default/index.html.twig');
+        $redis = $this->get('snc_redis.default');
+
+        // en premier paramètre nous passons la "clé" où sont nos utilisateurs.
+        // puis on veut tous les utilisateurs près de "user:3" sur un rayon de 10km.
+        $userNear = $redis->georadiusbymember(
+            'user_position',
+            'user:3',
+            10,
+            'km',
+            //Petite remarque n'oubliez pas de passer l'argument à true
+            ['WITHDIST' => true]
+        );
+
+        return $this->render('default/index.html.twig', ['userNear' => $userNear]);
     }
 }
